46b5445bc0a84a3c08507763833229c67c1d4a00
Caleb Barzee <barzeec@gmail.com>
2023-03-28 15:47:13 -0600

    lesson 3 personal project complete

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,7 @@
 const mongoDB = require("../config/db.js");
-const ObjectId = require("mongodb").ObjectId;
+const ObjectId = require("mongoDB").ObjectId;
 
+// GET
 const getAllContacts = async (req, res, next) => {
   const contacts = await mongoDB.getDB("contactsDB").collection("contacts").find({}).toArray();
   res.setHeader("Content-Type", "application/json");
@@ -17,5 +18,49 @@ const getContact = async (req, res, next) => {
   res.send(JSON.stringify(contact));
 };
 
-module.exports = { getAllContacts, getContact };
+// POST
+const createContact = async (req, res, next) => {
+  try {
+    const contacts = await mongoDB.getDB("contactsDB").collection("contacts").insertOne(req.body, (err) => {
+    !err ? res.status(201)
+    .json({ message: "Document created successfully", docId: contacts.insertedId.toString() }) 
+    : res.status(404)
+    .json({ message: "Error creating document." });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// DELETE
+const deleteContact = async (req, res, next) => {
+  try {
+    const id = new ObjectId(req.params.id);
+    await mongoDB.getDB("contactsDB").collection("contacts").deleteOne({ _id: id }, (err) => {
+      !err ? res.status(200)
+      .json({ message: `Document with ID: ${req.params.id} successfully deleted.` })
+      : res.status(404)
+      .json({ message: "Error deleting document." });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// PUT
+const updateContact = async (req, res, next) => {
+  try {
+    const id = new ObjectId(req.params.id);
+    await mongoDB.getDB("contactsDB").collection("contacts").updateOne({ _id: id }, { $set: req.body }, (err) => {
+        res.setHeader(`Content-Type`, `application/json`);
+        !err ? res.status(204).send()
+        : res.status(404)
+        .json({ message: "Error updating document." });
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = { getAllContacts, getContact, createContact, deleteContact, updateContact };
 
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -3,5 +3,8 @@ const contacts_controller = require("../controllers/contacts.js");
 
 router.get("", contacts_controller.getAllContacts);
 router.get("/:id", contacts_controller.getContact);
+router.post("/new", contacts_controller.createContact);
+router.delete("/:id", contacts_controller.deleteContact);
+router.put("/:id", contacts_controller.updateContact);
 
 module.exports = router;
